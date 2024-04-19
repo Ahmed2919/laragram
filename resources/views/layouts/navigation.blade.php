382b07c0cea3eb34eb88021573981f94f94e8983
dev <dev@test.com>
2024-04-19 07:48:12 +0300

    05-04-02 autherization

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,83 +15,75 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="sm:flex sm:items-center">
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @guest
                     <div class="hidden md:flex md:items-center md:space-x-2">
                         <div class="space-x-3 text-[1.6rem] ltr:mr-5 rtl:ml-5">
                             <a href="/login"
-                               class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ltr:mr-2 rtl:ml-2">{{ __('Login') }}</a>
+                            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ltr:mr-2 rtl:ml-2">{{ __('Login') }}</a>
                             <a href="/register"
-                               class="inline-flex items-center px-4 py-2 font-semibold text-xs uppercase tracking-widest">{{ __('Register') }}</a>
+                            class="inline-flex items-center px-4 py-2 font-semibold text-xs uppercase tracking-widest">{{ __('Register') }}</a>
                         </div>
                     </div>
                 @endguest
                 @auth
                     <div class="hidden md:flex md:flex-row space-x-3 items-center justify-center">
-                        {{-- Home --}}
-                        <a class="text-[1.6rem] rtl:ml-3" href="{{ route('home_page') }}">
-                            {!! url()->current() == route('home_page')
-                                ? '<i class="bx bxs-home-alt-2"></i>'
-                                : '<i class="bx bx-home-alt-2"></i>' !!}
-                        </a>
-
-                        {{-- Explore --}}
-                        <a class="text-[1.6rem]" href="{{ route('explore') }}">
-                            {!! url()->current() == route('explore') ? '<i class="bx bxs-compass"></i>' : '<i class="bx bx-compass"></i>' !!}
-                        </a>
-
-                        {{-- Create Post --}}
-                        <button onclick="Livewire.emit('openModal', 'create-post-modal')">
-                            <i class="bx bx-message-square-add text-[1.6rem]"></i>
-                        </button>
-                        <div class="hidden md:block">
-                            <x-dropdown align="right" width="96">
-                                <x-slot name="trigger">
-                                    <button class="text-[1.6rem] ltr:mr-2 rtl:ml-2 leading-5">
-                                        <div class="relative">
-                                            <i class="bx bxs-inbox"></i>
-                                            <livewire:pending-followers-count />
-                                        </div>
-                                    </button>
-                                </x-slot>
-
-                                <x-slot name="content">
-                                    <livewire:pending-followers-list />
-                                </x-slot>
-                            </x-dropdown>
+                        <div class="space-x-3 text-[1.6rem] mr-2 loading-5">
+                            <a href=" {{ route('home_page') }} ">
+                                {!! url()->current() == route('home_page')
+                                    ?'<i class="bx bxs-home-alt-2"></i>'
+                                    :'<i class="bx bx-home-alt-2"></i>'
+                                !!}
+                            </a>
+                            <a href="{{route('explore')}}">
+                                {!! url()->current() == route('explore')
+                                    ?'<i class="bx bxs-compass"></i>'
+                                    :'<i class="bx bx-compass"></i>'
+                                !!}
+                            </a>
+                            <a href="{{route('post.create')}}">
+                                {!! url()->current() == route('post.create')
+                                    ?'<i class="bx bxs-message-square-add"></i>'
+                                    :'<i class="bx bx-message-square-add"></i>'
+                                !!}
+                            </a>
                         </div>
-                        <div class="hidden md:block">
-                            <x-dropdown align="{{ app()->getLocale() == 'ar' ? 'left' : 'right' }}" width="48">
+                    </div>
+                    <div class="hidden md:block">
+                                
+                            <x-dropdown align="right" width="48">
+                        
                                 <x-slot name="trigger">
-                                    <button
-                                        class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                        <div>
-                                            <img class="w-6 h-6 -mt-1 object-cover rounded-full border border-gray-500"
-                                                 src="{{ auth()->user()->image }}">
+                                        <div class="ml-4">
+                                            <img src="{{asset('storage/'.auth()->user()->image) }}" class="rounded-full w-6 h-6"/>
                                         </div>
 
-                                    </button>
+                                    
                                 </x-slot>
 
                                 <x-slot name="content">
-                                    <x-dropdown-link :href="route('user_profile', auth()->user())">{{ __('Profile') }}</x-dropdown-link>
+                                    <x-dropdown-link :href="route('user_profile' , ['user' => auth()->user()])">
+                                        {{ __('Profile') }}
+                                    </x-dropdown-link>
+
                                     <!-- Authentication -->
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
 
                                         <x-dropdown-link :href="route('logout')"
-                                                         onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
                                             {{ __('Log Out') }}
                                         </x-dropdown-link>
                                     </form>
                                 </x-slot>
                             </x-dropdown>
-                        </div>
+
                     </div>
+                @endauth
 
             </div>
-            @endauth
+
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -110,6 +102,9 @@
             <x-responsive-nav-link :href="route('home_page')" :active="request()->routeIs('home_page')">
                 {{ __('Home') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('explore')">{{ __('Explore') }}</x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('post.create')">{{ __('New Post') }}</x-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
